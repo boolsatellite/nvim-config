b8c12f8c3648978c9330c03dceb67a67f3414ba1
boolsatellite <boolsatellite@gmail.com>
2025-05-13 18:52:57 +0800

    perf: change bufferline keymap - remove <leader>

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,15 +4,15 @@ return {
 
   keys = {
     -- Buffer pick / Buffer seek
-    { "<leader>bs", "<Cmd>BufferLinePick<CR>", desc = "Pick buffer" },
+    { "bs", "<Cmd>BufferLinePick<CR>", desc = "Pick buffer" },
 
     -- 切换到下一个 / 上一个 buffer
     { "L", "<Cmd>BufferLineCycleNext<CR>", desc = "Next buffer" },
     { "H", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev buffer" },
 
     -- 关闭当前 buffer；<leader>bo 关闭其它所有 buffers
-    { "<leader>bd", "<Cmd>bdelete<CR>", desc = "Kill buffer" },
-    { "<leader>bo", "<Cmd>BufferLineCloseLeft<CR><Cmd>BufferLineCloseRight<CR>", desc = "Kill other buffers" },
+    { "bd", "<Cmd>bdelete<CR>", desc = "Kill buffer" },
+    { "bo", "<Cmd>BufferLineCloseLeft<CR><Cmd>BufferLineCloseRight<CR>", desc = "Kill other buffers" },
   },
 
   config = function()
